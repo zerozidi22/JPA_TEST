991fe4675846836d162c2820d191f5eb48d87c7d
Herny <Henry.kim@kaonsoft.com>
2020-07-25 22:43:44 +0900

    init

---
+++ src/main/java/com/example/JPA_TEST/JpaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.JPA_TEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JPA_TEST/JpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPA_TEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
